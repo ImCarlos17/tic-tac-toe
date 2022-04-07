6151f64d30108f2f6394195d5329f017991202ee
Carlos Barreto <carlosmba96@gmail.com>
2022-04-07 08:46:43 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-var moduleGame = (() => {
-  const gameBoard = [];
+const moduleGame = (() => {
+  const spots = document.querySelectorAll(".position");
 
-  const Gamerboard = { gameBoard };
+  const gameBoard = [...Array.from(spots)];
+
+  spots.forEach((spot, index) => {
+    spot.addEventListener("click", function eventClick(e) {
+      playerXturn(e.target, index);
+    });
+  });
+
+  const playingTurn = () => {};
+
+  const playerXturn = () => {
+    target.textContent = "O";
+  };
+
+  const playerOturn = () => {
+    target.textContent = "O";
+  };
+
+  return {};
 })();
