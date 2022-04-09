2ae23f0df3d66abae743c2ebeff345f5d4f054ba
Carlos Barreto <carlosmba96@gmail.com>
2022-04-09 11:57:01 -0400

    update

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 const moduleGame = (() => {
+  const titleTurn = document.querySelector("#player-turn");
   const spots = document.querySelectorAll(".position");
+  const gameBoard = Array.from(spots).map((spot) => spot.textContent);
+  let newGameBoard = [...gameBoard];
 
-  const gameBoard = [...Array.from(spots)];
+  const Players = (player, marker) => {
+    return { player, marker };
+  };
 
-  spots.forEach((spot, index) => {
-    spot.addEventListener("click", function eventClick(e) {
-      playerXturn(e.target, index);
-    });
+  const player1 = Players("player1", "O");
+  const player2 = Players("player2", "X");
+
+  let currentPlayer = player1;
+
+  spots.forEach((spot) => {
+    spot.addEventListener("click", startGame);
   });
 
-  const playingTurn = () => {};
+  function startGame(e) {
+    let spot = e.target;
+    let position = e.target.dataset.spot;
 
-  const playerXturn = () => {
-    target.textContent = "O";
+    renderPlayerTurn(currentPlayer.marker);
+    controllerTurn();
+    addMarkerToBoard(spot, position, currentPlayer.marker);
+  }
+
+  const renderPlayerTurn = (currentPlayer) => {
+    titleTurn.textContent = `Player ${currentPlayer}'s turn`;
   };
 
-  const playerOturn = () => {
-    target.textContent = "O";
+  const controllerTurn = () => {
+    currentPlayer == player1
+      ? (currentPlayer = player2)
+      : (currentPlayer = player1);
   };
 
-  return {};
+  const addMarkerToBoard = (spot, position, playerMarker) => {
+    newGameBoard[position] = playerMarker;
+    console.log(newGameBoard);
+  };
 })();
