38ee5dfdf1e128f01d4f8ae459ed14274278dff2
Carlos Barreto <carlosmba96@gmail.com>
2022-04-10 13:46:39 -0400

    project finished

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const moduleGame = (() => {
   const titleTurn = document.querySelector("#player-turn");
   const spots = document.querySelectorAll(".position");
+  const btnRestart = document.querySelector(".btn-restart");
   const gameBoard = Array.from(spots).map((spot) => spot.textContent);
   let newGameBoard = [...gameBoard];
+
+  btnRestart.addEventListener("click", resetGame);
+
+  const gameOver = () => {
+    let _isGameOver = false;
+    const get = () => _isGameOver;
+    const set = (value) => {
+      _isGameOver = value;
+    };
+
+    return {
+      get,
+      set,
+    };
+  };
+
+  const newGameOver = gameOver();
 
   const Player = (player, marker) => {
     return { player, marker };
@@ -17,15 +35,21 @@ const moduleGame = (() => {
     spot.addEventListener("click", startGame);
   });
 
+  titleTurn.textContent = `Player ${currentPlayer.marker}'s turn`;
+
   function startGame(e) {
     let spot = e.target;
     let position = e.target.dataset.spot;
 
-    renderPlayerTurn(currentPlayer.marker);
-    addMarkerToBoard(position, currentPlayer.marker);
-    controllerTurn();
-    renderMarkerToBoard(spot, currentPlayer.marker);
-    chooseWinner(newGameBoard);
+    if (!spot.textContent && !newGameOver.get()) {
+      addMarkerToBoard(position, currentPlayer.marker);
+      renderMarkerToBoard(spot, currentPlayer.marker);
+      conditionsToWinPlayer(newGameBoard, currentPlayer.marker);
+      controllerTurn();
+      if (!newGameOver.get()) {
+        renderPlayerTurn(currentPlayer.marker);
+      }
+    }
   }
 
   const renderPlayerTurn = (currentPlayer) => {
@@ -34,6 +58,7 @@ const moduleGame = (() => {
 
   const addMarkerToBoard = (position, playerMarker) => {
     newGameBoard[position] = playerMarker;
+    console.log(newGameBoard);
   };
 
   const renderMarkerToBoard = (spot, marker) => (spot.textContent = marker);
@@ -44,5 +69,40 @@ const moduleGame = (() => {
       : (currentPlayer = player1);
   };
 
-  const chooseWinner = (board) => {};
+  const renderPlayerWinner = (playerMarker) => {
+    titleTurn.textContent = `Player ${playerMarker}'s Win`;
+  };
+
+  const conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const conditionsToWinPlayer = (board, playerMarker) => {
+    const arePlaysAvailable = board.some((spot) => spot === "");
+
+    if (arePlaysAvailable) {
+      conditions.forEach((condition) => {
+        if (condition.every((position) => board[position] === playerMarker)) {
+          renderPlayerWinner(playerMarker);
+          newGameOver.set(true);
+        }
+      });
+    } else {
+      renderPlayerWinner("draw");
+      newGameOver.set(true);
+    }
+  };
+
+  function resetGame() {
+    newGameBoard = gameBoard;
+    spots.forEach((spot) => (spot.textContent = ""));
+    newGameOver.set(false);
+  }
 })();
